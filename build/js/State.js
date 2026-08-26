dd4fb062d567453b4bca3a350d6385b5c1cd4f21
deballap-webdev <debbieallap@gmail.com>
2026-08-26 15:44:27 +0100

    add getters and setters to classes

--- build/js/State.js
+++ build/js/State.js
@@ -11,22 +11,158 @@ export class ExchangeData {
     this.#interval = "1M";
     this.#log = [];
   }
+
+  getCurerntBase() {
+    return this.#currentBase;
+  }
+
+  getCurerntQuote() {
+    return this.#currentQuote;
+  }
+
+  getFavorite() {
+    return this.#favorite;
+  }
+
+  getInterval() {
+    return this.#interval;
+  }
+
+  getLog() {
+    return this.#log;
+  }
+
+  getInterval() {
+    return this.#interval;
+  }
+
+  clearLog() {
+    const log = this.#log;
+    log.splice(0, log.length);
+  }
+
+  clearFavorite() {
+    const favorite = this.#favourite;
+    favorite.splice(0, favorite.length);
+  }
+
+  removePairFromFavorite(pairId) {
+    const favorite = this.#favorite;
+    removeItemFromArray(favorite, pairId);
+  }
+
+  addConvToLog(conv) {
+    const log = this.#log;
+    log.push(conv);
+  }
+
+  addPairToFavorite(pair) {
+    const favorite = this.#favorite;
+    favorite.push(pair);
+  }
+
+  removeConvFromLog(logId) {
+    const log = this.#log;
+    removeItemFromArray(log, logId);
+  }
+
+  setExhangeData(exchangeObj) {
+    const { log, favorite, interval, currentBase, currentQuote } = exchangeObj;
+    if (log) this.#log = log;
+    if (favorite) this.#favorite = favorite;
+    if (interval) this.#interval = interval;
+    if (currentBase) this.#currentBase = currentBase;
+    if (currentQuote) this.#currentQuote = currentQuote;
+  }
 }
 
+const removeItemFromArray = (array, itemId) => {
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].getId() === itemId) {
+      array.splice(i, 1);
+      break;
+    }
+  }
+};
+
 export class FavPair {
+  #base;
+  #quote;
+  #id;
   constructor() {
-    this.base = null;
-    this.quote = null;
+    this.#base = null;
+    this.#quote = null;
+    this.#id = null;
+  }
+
+  setFavPair(favPairObj) {
+    const { base, quote, id } = favPairObj;
+    this.#base = base;
+    this.#quote = quote;
+    this.#id = id;
+  }
+
+  getBase() {
+    return this.#base;
+  }
+
+  getQuote() {
+    return this.#quote;
+  }
+
+  getId() {
+    return this.#id;
   }
 }
 
 export class LoggedConv {
+  #base;
+  #quote;
+  #date;
+  #send;
+  #receive;
+  #id;
   constructor() {
-    this.base = null;
-    this.quote = null;
-    this.date = null;
-    this.send = null;
-    this.receive = null;
+    this.#base = null;
+    this.#quote = null;
+    this.#date = null;
+    this.#send = null;
+    this.#receive = null;
+    this.#id = null;
+  }
+
+  getBase() {
+    return this.#base;
+  }
+
+  getQuote() {
+    return this.#quote;
+  }
+
+  getDate() {
+    return this.#date;
+  }
+
+  getSendAmount() {
+    return this.#send;
+  }
+
+  getReceiveAmount() {
+    return this.#receive;
+  }
+
+  getId() {
+    return this.#id;
+  }
+
+  setLoggedConv(convObj) {
+    const { base, quote, date, send, receive, id } = convObj;
+    this.#base = base;
+    this.#date = date;
+    this.#quote = quote;
+    this.#receive = receive;
+    this.#send = send;
+    this.#id = id;
   }
 }
 
--- build/js/main.js
+++ build/js/main.js
@@ -165,14 +165,6 @@ const handleMobileNav = (event) => {
   dropDownDisplay(event, mobileNav, mobileNavBtn);
 };
 
-const fetchCurrencyData = async () => {
-  const currencyStream = await fetch(
-    "https://api.frankfurter.dev/v2/rates?base=USD&quotes=USD,EUR,GBP,JPY,AUD,CAD,CHF,CNY,NZD,HKD,SGD,SEK,NOK,DKK,KRW,INR,BRL,ZAR,MXN,IDR,TRY,SAR,AED,THB,MYR,PHP,PLN,ILS,ARS,CLP,COP,EGP,NGN,PKR,VND,TWD,CZK,HUF,RON,BGN,ISK,HRK,UAH,KWD,QAR,OMR,BHD,KZT,MAD,PEN,UYU,CRC,JOD,LBP,RUB",
-  );
-  const currencyJson = await currencyStream.json();
-  console.log(currencyJson);
-};
-
 fetchCurrencyData();
 
 document.addEventListener("DOMContentLoaded", initApp);
